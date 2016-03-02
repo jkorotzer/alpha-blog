9b569cd33002075ce416af3a52ce4cc495c2e071
jkorotzer <jkorotzer@wustl.edu>
2016-03-02 00:23:12 +0000

    added some actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+  def index
+    @all_articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -12,10 +20,19 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
-  def show 
+
+  def show
     @article = Article.find(params[:id])
-    
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
